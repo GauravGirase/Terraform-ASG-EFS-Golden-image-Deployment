2e0f1954ebfa28167731bafcb1e9d70b089a9292
gaurav <gaurav.girase111@gmail.com>
2026-02-08 16:59:11 +0530

    added static website reference in ansible playbook

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,6 +103,14 @@ resource "aws_security_group" "ec2_sg" {
     security_groups = [aws_security_group.alb_sg.id]
   }
 
+  # To test website is live
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Added for debugging purpose
   ingress {
     from_port   = 22
