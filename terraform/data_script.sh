ea79299c9a314ab6369025eb00f31547ad8a9989
gaurav <gaurav.girase111@gmail.com>
2026-02-08 14:15:21 +0530

    fixed typp

--- terraform/data_script.sh
+++ terraform/data_script.sh
@@ -8,9 +8,32 @@ efs_dns: ${efs_dns}
 EOT
 
 # Install Ansible
-yum install -y amazon-linux-extras
-amazon-linux-extras enable ansible2
-yum install -y ansible git
+sudo yum install -y amazon-linux-extras
+sudo amazon-linux-extras enable ansible2
+sudo yum install -y ansible git
+
+# Install nginx
+sudo amazon-linux-extras enable nginx1
+sudo yum install -y python3-botocore
+sudo yum install -y nginx
+
+sudo mkdir -p /mnt/efs/current
+sudo mkdir -p /etc/nginx/conf.d/
+
+# Write new webapp config
+cat << 'EOF' | sudo tee /etc/nginx/conf.d/webapp.conf
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /mnt/efs/current;
+    index index.html;
+
+    location / {
+        try_files $uri $uri/index.html =404;
+    }
+}
+EOF
 
 # Run Ansible (pull model)
-ansible-pull -U https://github.com/GauravGirase/Terraform-ASG-EFS-Golden-image-Deployment.git playbooks/efs.yml
+sudo ansible-pull -U https://github.com/GauravGirase/Terraform-ASG-EFS-Golden-image-Deployment.git playbooks/efs.yml
