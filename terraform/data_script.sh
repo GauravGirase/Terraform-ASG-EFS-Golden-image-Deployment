0cc065eddc27bd529ccf993115bc58e32341ceea
gaurav <gaurav.girase111@gmail.com>
2026-02-08 13:52:22 +0530

    fixed typp

--- terraform/data_script.sh
+++ terraform/data_script.sh
@@ -3,8 +3,8 @@ set -euxo pipefail
 
 # Persist EFS info (from Terraform vars)
 cat <<EOT >/etc/efs.env
-efs_id: ${efs_id}"
-efs_dns: "${efs_dns}"
+efs_id: ${efs_id}
+efs_dns: ${efs_dns}
 EOT
 
 # Install Ansible
@@ -13,6 +13,4 @@ amazon-linux-extras enable ansible2
 yum install -y ansible git
 
 # Run Ansible (pull model)
-ansible-pull \
--U https://github.com/GauravGirase/Terraform-ASG-EFS-Golden-image-Deployment.git \
-playbooks/efs.yml
+ansible-pull -U https://github.com/GauravGirase/Terraform-ASG-EFS-Golden-image-Deployment.git playbooks/efs.yml
